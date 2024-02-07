28ca6f13cbce94bce8640b18664aed4794f07cce
Savio Ferreira <saviofc.dev@gmail.com>
2024-02-06 23:24:37 -0300

    Text - Rn

---
+++ StayWithMe/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { View, Text } from 'react-native';
+import Basic from "./componentes/Basic";
+
+export default function App() {
+    return (
+        <Basic texto = "¬"/>
+    )
+}
--- StayWithMe/src/componentes/basic.js
+++ StayWithMe/src/componentes/basic.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { cloneElement } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function (props) {
-     return <Text>{props.text}</Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.mainText}>{props.texto}Aqui estou mais um dia</Text>
+        </View>
+
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'black'
+    },
+    mainText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color : "purple",
+    }
+});
