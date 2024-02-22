9454e62e7b8b6cde97717a5d73eb8be45addc306
Savio Ferreira <saviofc.dev@gmail.com>
2024-02-21 23:18:34 -0300

    tentativa de rebase

--- projeto01/App.tsx
+++ projeto01/App.tsx
@@ -1,26 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.fontColor}>Main</Text>
-      <StatusBar style="light" />
-    </View>
+   <>
+   </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#103',
-    
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  fontColor: {
-    color: 'purple',
-    fontSize: 40,
-    margin: 10,
-  },
-});
