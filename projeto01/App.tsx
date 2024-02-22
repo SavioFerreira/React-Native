8937ae301d258f497cbb9e55d0edd6482de4c5be
Savio Ferreira <saviofc.dev@gmail.com>
2024-02-21 23:14:54 -0300

    testando icones

--- projeto01/App.tsx
+++ projeto01/App.tsx
@@ -1,8 +1,14 @@
-import { View } from "react-native";
+import { StatusBar } from "react-native";
 import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-      <Home/>
+    <>
+      <StatusBar  
+      barStyle={'light-content'}
+      backgroundColor='transparent'
+      translucent/>
+      <Home />
+    </>
   )
 }
--- projeto01/src/screens/Home/index.tsx
+++ projeto01/src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Text, TextInput, Image, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { View, Text, TextInput, Image, TouchableOpacity,  } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 
 const logo_World = '../../../assets/logo-world.png';
 export default function Home() {
+
     return (
         <View style={styles.container}>
             <TouchableOpacity>
@@ -14,11 +16,13 @@ export default function Home() {
                 <TextInput
                     style={styles.inputText}
                     placeholder='Email:'
-                    placeholderTextColor={'#00FFFF'} inputMode='email' />
+                    placeholderTextColor={'#00FFFF'} inputMode='email' 
+                    />
                 <TextInput
                     style={styles.inputText}
                     placeholder='Senha:'
                     placeholderTextColor={'#00FFFF'} />
+                    <Icon name='user-secret' size={30}/>
             </View>
         </View>
     )
