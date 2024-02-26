faae044e95dc58c3671977a8c914a1cb9eddf516
Savio Ferreira <saviofc.dev@gmail.com>
2024-02-25 23:59:28 -0300

    mensagem oculta

--- projeto01/src/screens/Home/index.tsx
+++ projeto01/src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, Image, TouchableOpacity, } from 'react-native';
+import { View, Text, TextInput, Image, TouchableOpacity, Alert, } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Icon2 from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
@@ -7,9 +7,27 @@ import styles from './styles';
 const logo_World = '../../../assets/logo-world.png';
 export default function Home() {
 
+    function showColetaColetivaLongPress() {
+        return (
+            Alert.alert('Coleta Coletiva - SFC',
+                'App Feito com react Native no mobile e Java spring no backEnd.\n' +
+                'O banco de dados utilizado foi o postgress e e RealmDB\n' +
+                'A meta é construir todo o app até o final de maio, para que seja possível apresentar' + 
+                'a banca até o dia marcado em junho.')
+        )
+    }
+    
+    function showColetaColetivaPress() {
+        return (
+            Alert.alert('Coleta Coletiva',
+                'App Coleta coletiva\nProjeto final de curso\nSávio Ferreira')
+        )
+    }
     return (
         <View style={styles.container}>
-            <TouchableOpacity>
+            <TouchableOpacity 
+                onLongPress={showColetaColetivaLongPress}
+                onPress={showColetaColetivaPress}>
                 <Image style={styles.logo} source={require(logo_World)} />
                 <Text style={styles.textMain}>Coleta Coletiva</Text>
             </TouchableOpacity>
@@ -19,6 +37,7 @@ export default function Home() {
                         style={styles.inputText}
                         placeholder='Email:'
                         placeholderTextColor={'#00FFFF'} inputMode='email'
+
                     />
                     <Icon style={styles.loginIcon} name='user-circle' />
                 </View>
@@ -33,5 +52,6 @@ export default function Home() {
 
             </View>
         </View>
+
     )
 }
