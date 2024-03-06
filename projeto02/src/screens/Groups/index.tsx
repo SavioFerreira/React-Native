aba856841d1272c2a330c6bc394447e08c5b04d3
Savio Ferreira <saviofc.dev@gmail.com>
2024-03-05 22:13:41 -0300

    Componente Highlight - para titulo e subtitulo - criando tipagem com ts.

--- projeto02/src/screens/Groups/index.tsx
+++ projeto02/src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
-
+import { Highlight } from '@components/Highlight';
 export function Groups() {
     return (
       <Container>
         <Header />
+        <Highlight title='O senhor dos Aneis' subtitle='lá ele, só se for o seu'/>
       </Container>
     );
   }
