b24fdde1c7cb9fc88371b71d1b63424043a5c2ae
Savio Ferreira <saviofc.dev@gmail.com>
2024-03-06 23:00:55 -0300

    componente dentro de outro

--- projeto02/src/screens/Groups/index.tsx
+++ projeto02/src/screens/Groups/index.tsx
@@ -1,15 +1,29 @@
-import { Header } from '@components/Header';
 import { Container } from './styles';
+import { useState } from 'react';
+import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
 
 export function Groups() {
-    return (
-      <Container>
-        <Header />
-        <Highlight title='O senhor dos Aneis' subtitle='lá ele, só se for o seu' />
-        <GroupCard title='Lacrimosa' />
-      </Container>
-    );
-  }
-  
+  const [groups, setGroups] = useState<string[]>(['Pamonha Frita', 'Correria', 'Propraroxitona']);
+
+  return (
+    <Container>
+      <Header />
+      <Highlight
+        title='O senhor dos Aneis'
+        subtitle='lá ele, só se for o seu'
+      />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
+    </Container>
+  );
+}
