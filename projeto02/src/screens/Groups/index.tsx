c2e9ee20cb2cf532089f39a5509c481cd8de88c2
Savio Ferreira <saviofc.dev@gmail.com>
2024-03-06 22:32:02 -0300

    Criando tipagem com propriedades de componentes.

--- projeto02/src/screens/Groups/index.tsx
+++ projeto02/src/screens/Groups/index.tsx
@@ -1,11 +1,14 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+
 export function Groups() {
     return (
       <Container>
         <Header />
-        <Highlight title='O senhor dos Aneis' subtitle='lá ele, só se for o seu'/>
+        <Highlight title='O senhor dos Aneis' subtitle='lá ele, só se for o seu' />
+        <GroupCard title='Lacrimosa' />
       </Container>
     );
   }
