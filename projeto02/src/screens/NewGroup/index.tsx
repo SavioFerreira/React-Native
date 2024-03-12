49691c3ec20f8605f0d7e91ab4faa199384d4b3e
Savio Ferreira <saviofc.dev@gmail.com>
2024-03-12 20:01:01 -0300

    Reutilizando componentes na nova tela.

--- projeto02/src/screens/NewGroup/index.tsx
+++ projeto02/src/screens/NewGroup/index.tsx
@@ -1,9 +1,29 @@
-import { Container } from './styles'
+import styled from 'styled-components/native';
+import { Container, Content } from './styles'
+import { Header } from '@components/Header';
+import { UsersThree } from 'phosphor-react-native';
+import { Highlight } from '@components/Highlight';
+import { Button } from '@components/Button';
+
 
 export function NewGroup() {
     return (
         <Container> 
-        
+            <Header showBackButton/>
+            <Content>
+              <Icon />
+              <Highlight 
+                title='Nova Denúnia'
+                subtitle='Criar uma nova denúncia'/>
+                <Button title='Criar'/>
+            </Content>
         </Container>
     );
 }
+
+export const Icon = styled(UsersThree).attrs(({ theme }) => ({
+  size: 56,
+  color: theme.COLORS.GREEN_700
+}))`
+  align-self: center;
+`;
