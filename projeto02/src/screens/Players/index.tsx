203530910bab6f4b25aa954920a306fc17df0fe9
Savio Ferreira <saviofc.dev@gmail.com>
2024-03-14 22:08:36 -0300

    componente IconButton - Criando Tipagem com lib de Icons

--- projeto02/src/screens/Players/index.tsx
+++ projeto02/src/screens/Players/index.tsx
@@ -1,14 +1,23 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
 
 export function Players() {
     return (
         <Container>
           <Header showBackButton />
           <Highlight 
-            title='Nome do Grupo'
-            subtitle='nome dos componentes da coleta coletiva'/>
+            title='Denúncias Group'
+            subtitle='nome dos componentes da coleta coletiva'
+          />
+          <Input 
+            placeholder='Nome da pessoa'
+          />
+          <ButtonIcon 
+            icon='add'
+          />
         </Container>
     );
 }
