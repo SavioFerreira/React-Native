283421c848fb145f339687f1b43c90520f7f5296
Savio Ferreira <saviofc.dev@gmail.com>
2024-03-18 22:36:36 -0300

    Finalizando componente PLayer.

--- projeto02/src/screens/Players/index.tsx
+++ projeto02/src/screens/Players/index.tsx
@@ -6,20 +6,24 @@ import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 import { FlatList } from 'react-native';
 import { useState } from 'react';
+import { PlayersCard } from '@components/PlayersCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Players() {
-  const [team, setTeam] = useState<string>();
-  const [players, setPlayers] =useState([]);
+  const [team, setTeam] = useState('Coleta 0');
+  const [players, setPlayers] = useState(['primeiro', 'segundo']);
+  
   return (
     <Container>
       <Header showBackButton />
       <Highlight
-        title='Denúncias Group'
+        title='Denúncias Groups'
         subtitle='nome dos componentes da coleta coletiva'
       />
       <Form>
         <Input
-          placeholder='Nome da pessoa'
+          placeholder='Nome do membro'
           autoCorrect={false}
         />
         <ButtonIcon
@@ -28,7 +32,7 @@ export function Players() {
       </Form>
       <HeaderList>
         <FlatList
-          data={['coleta 01', 'coleta 02', 'coleta 03']}
+          data={['coleta 01', 'coleta 02']}
           keyExtractor={item => item}
           renderItem={({ item }) => (
             <Filter 
@@ -41,7 +45,27 @@ export function Players() {
          /> 
          <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
-      
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayersCard 
+            name={item}
+            onRemove={() => {}} 
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Não há pessoas na coleta'
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[{ paddingBottom : 100 }, players.length === 0 && { flex: 1 }]}
+      />
+      <Button 
+        title='Remover membro'
+        type='SECONDARY'
+      />  
     </Container>
   );
 }
