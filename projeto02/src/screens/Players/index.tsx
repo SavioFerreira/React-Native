3db44927d098ad648dafbb769ed1c37fcb234fe2
Savio Ferreira <saviofc.dev@gmail.com>
2024-03-30 18:51:02 -0300

    storage para players - getAll e getByGroup

--- projeto02/src/screens/Players/index.tsx
+++ projeto02/src/screens/Players/index.tsx
@@ -4,12 +4,15 @@ import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 import { useState } from 'react';
 import { PlayersCard } from '@components/PlayersCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 import { useRoute } from '@react-navigation/native';
+import { AppError } from '@utils/AppError';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playerGetByGroup } from '@storage/player/playersGetByGroup';
 
 
 type RouteParams = {
@@ -17,12 +20,35 @@ type RouteParams = {
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('');
   const [team, setTeam] = useState('');
   const [players, setPlayers] = useState(['']);
 
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0){
+      return Alert.alert('Novo membro', 'Informe o nome do usuário.')
+    }
+    const NewPlayer = {
+      name: newPlayerName,
+      team,
+    }
+    try {
+      await playerAddByGroup(NewPlayer, group);
+      const players =  await playerGetByGroup(group);
+      console.log(players);
+    } catch(error) {
+      if(error instanceof AppError) {
+        Alert.alert('Novo membro', error.message)
+      } else {
+        console.log(error);
+        Alert.alert('Novo membro', 'Nao foi possível adicionar');
+      }
+    }
+  }
   return (
     <Container>
       <Header showBackButton />
@@ -32,11 +58,13 @@ export function Players() {
       />
       <Form>
         <Input
+        onChangeText={setNewPlayerName}
           placeholder='Nome do membro'
           autoCorrect={false}
         />
         <ButtonIcon
           icon='add'
+          onPress={handleAddPlayer}
         />
       </Form>
       <HeaderList>
--- projeto02/src/storage/storageConfig.ts
+++ projeto02/src/storage/storageConfig.ts
@@ -1,3 +1,5 @@
 const GROUP_COLLECTION = '@denuncias-teams:groups';
+const PLAYER_COLLECTION = '@denuncias-teams:players';
 
 export { GROUP_COLLECTION };
+export { PLAYER_COLLECTION };
