8e85bf354448fc91b79ad2e956664da04e440483
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-02 15:50:43 -0300

    Criando o projeto 03  e incluindos fonte Roboto

---
+++ projeto03/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+export default function App() {
+  const [fontsLoaded]  = useFonts({Roboto_400Regular, Roboto_700Bold})
+  return (
+    <View >
+      {fontsLoaded ? <Text>Hello world</Text> : <View/>}
+      <StatusBar style='auto'/>
+    
+    </View>
+  );
+}
