e20b1cc913e3181f9d59337e1d20c4e2f390ba9f
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-03 16:30:11 -0300

    incluindo tela de lignUp e dependências de navegação entre telas de login

---
+++ projeto03/src/screens/SignUp.tsx
@@ -0,0 +1,53 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+
+import LogoSvg from '@assets/logo.svg'
+import  BackgroundImg  from '@assets/background.png';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+export function SignUp() {
+  return (
+    <ScrollView contentContainerStyle={{flexGrow: 1}} showsHorizontalScrollIndicator={false}>
+      <VStack flex={1} bg={'gray.700'} px={10} pb={16}>
+        <Image 
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading  color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
+          <Input 
+            placeholder="Nome"
+          />
+          <Input 
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input 
+            placeholder="Senha"
+            secureTextEntry
+          />
+          <Button title="Criar e acessar"/>
+        </Center>
+
+        <Button 
+          title="Voltar para o login"
+          variant="outline"
+          mt={24}
+        />
+        
+      </VStack>
+    </ScrollView>
+  );
+}
