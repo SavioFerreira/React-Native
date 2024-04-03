84eb2ff7640a0b13093ed820ba91ba2916dd9a28
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-03 15:22:58 -0300

    _focus - input.

--- projeto03/src/components/Input.tsx
+++ projeto03/src/components/Input.tsx
@@ -1,19 +1,24 @@
 import { Input as NativeBaseInput, IInputProps } from 'native-base';
 
-export function Input({...rest}: IInputProps) {
-    return (
-        <NativeBaseInput 
-          bg="gray.700"
-          h={14}
-          w={350}
-          px={4}
-          borderWidth={0}
-          fontSize="md"
-          color="white"
-          fontFamily="body"
-          mb={4}
-          placeholderTextColor="gray.300"
-          {...rest}
-        />
-    );
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput 
+      bg="gray.700"
+      h={14}
+      w={360}
+      px={4}
+      borderWidth={0}
+      fontSize="md"
+      color="white"
+      fontFamily="body"
+      mb={4}
+      placeholderTextColor="gray.300"
+      _focus={{
+        bgColor: "gray.700",
+        borderWidth: 1,
+        borderColor: "green.500"
+      }}
+      {...rest}
+    />
+  );
 }
