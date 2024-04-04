9496d48880090ebf36a647d71d51fcde09defa04
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-04 11:34:40 -0300

    listando histórico com sessao - SectionList + native-base

--- projeto03/src/screens/history.tsx
+++ projeto03/src/screens/history.tsx
@@ -1,15 +1,38 @@
-import { Center, VStack } from 'native-base';
+import { Heading, VStack, SectionList } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
+import { useState } from 'react';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "04.04.24",
+      data: ["puxada frontal", "Remada unilateral"]
+    },
+    {
+      title: "04.04.24",
+      data: ["puxada frontal"]
+    }
+
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
     )
 }
