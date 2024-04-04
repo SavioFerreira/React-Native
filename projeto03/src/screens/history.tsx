34be6f40209ca039a3ac12ba341858e16fbfc403
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-04 11:51:49 -0300

    finalizando tela de historico

--- projeto03/src/screens/history.tsx
+++ projeto03/src/screens/history.tsx
@@ -1,8 +1,8 @@
-import { Heading, VStack, SectionList } from 'native-base';
+import { useState } from 'react';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
-import { useState } from 'react';
 
 export function History() {
   const [exercises, setExercises] = useState([
@@ -14,7 +14,6 @@ export function History() {
       title: "04.04.24",
       data: ["puxada frontal"]
     }
-
   ])
 
   return (
@@ -32,6 +31,14 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={[].length === 0 && {flex: 1, justifyContent: 'center'}}
+        ListEmptyComponent={() => (
+            <Text color="gray.100" textAlign="center">
+              Não há exercícios registrados ainda. {'\n'}
+              Vamos fazer exercícios hoje?
+            </Text>
+        )}
+        showsVerticalScrollIndicator={false}
       />
     </VStack>
     )
