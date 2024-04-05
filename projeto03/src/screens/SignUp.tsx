1e99c741d813add0c56441bcb819a1ebb9465381
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-05 10:53:12 -0300

    Adicionando mensagem de erro e estilização para inputs - Form control

--- projeto03/src/screens/SignUp.tsx
+++ projeto03/src/screens/SignUp.tsx
@@ -1,28 +1,33 @@
-import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { useNavigation } from "@react-navigation/native";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
 
-import LogoSvg from '@assets/logo.svg'
-import  BackgroundImg  from '@assets/background.png';
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
 
-import { Input } from '@components/Input';
-import { Button } from '@components/Button';
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
 
 export function SignUp() {
-  const {control} = useForm();
-
+  
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
   const navigation = useNavigation();
-
   function handleGoBack() {
     navigation.goBack();
   }
-
-  function handleSignUp() {
-    
+  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+    console.log({ name, email, password, password_confirm })
   }
   return (
-    <ScrollView contentContainerStyle={{flexGrow: 1}} showsHorizontalScrollIndicator={false}>
-      <VStack flex={1} px={10} pb={16}>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <VStack flex={1} px={10} pb={16}>
         <Image 
           source={BackgroundImg}
           defaultSource={BackgroundImg}
@@ -33,75 +38,89 @@ export function SignUp() {
         <Center my={24}>
           <LogoSvg />
           <Text color="gray.100" fontSize="sm">
-            Treine sua mente e seu corpo
+            Treine sua mente e o seu corpo.
           </Text>
         </Center>
-
         <Center>
-          <Heading  color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
           <Controller 
             control={control}
             name="name"
-            render={({ field: {onChange, value}}) => (
+            rules={{
+              required: 'Informe o nome.'
+            }}
+            render={({ field: { onChange, value } }) => (
               <Input 
                 placeholder="Nome"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.name?.message}
               />
             )}
           />
 
-          <Controller
-           control={control}
-           name="email"
-           render={({field: {onChange, value}}) => (
-             <Input 
-               placeholder="E-mail"
-               keyboardType="email-address"
-               autoCapitalize="none"
-               onChangeText={onChange}
-               value={value}
-             />
-           )}
+          <Controller 
+            control={control}
+            name="email"
+            rules={{
+              required: 'Informe o email.',
+              pattern: {
+                value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido'
+              }
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="E-mail" 
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
 
           <Controller 
             control={control}
             name="password"
-            render={({field: {onChange, value}}) => (
+            render={({ field: { onChange, value } }) => (
               <Input 
-                placeholder="senha"
+                placeholder="Senha" 
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
               />
             )}
           />
-
           <Controller 
             control={control}
             name="password_confirm"
-            render={({field: {onChange, value}}) => (
+            render={({ field: { onChange, value } }) => (
               <Input 
-                placeholder="Confirme a senha"
+                placeholder="Confirmar a Senha" 
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
               />
             )}
           />
-          <Button title="Criar e acessar"/>
+          <Button 
+            title="Criar e acessar" 
+            onPress={handleSubmit(handleSignUp)}
+          />
         </Center>
-
+        
         <Button 
-          title="Voltar para o login"
-          variant="outline"
+          title="Voltar para o login" 
+          variant="outline" 
           mt={24}
           onPress={handleGoBack}
         />
-        
       </VStack>
     </ScrollView>
   );
