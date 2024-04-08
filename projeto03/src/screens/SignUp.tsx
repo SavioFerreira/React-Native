ac96cf57a6777154efac5827c9d042e16ff8683e
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-08 10:28:24 -0300

    Criando mensagem personalizada com interceptores do Axios e chamando mensagem do back

--- projeto03/src/screens/SignUp.tsx
+++ projeto03/src/screens/SignUp.tsx
@@ -4,11 +4,15 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
+import axios from "axios";
+import { api } from '@services/api';
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -36,16 +40,26 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({name, email, password}: FormDataProps) {
-     
-     
-     toast.show({
-      title: `Obrigado, ${name}. Conta criada com sucesso.`,
-      placement: 'top',
-      bgColor: 'green.600',
+  async function handleSignUp({name, email, password}: FormDataProps) {
+    try {
+      const response = await api.post('/users', {name, email, password} );
+       toast.show({
+        title: `Obrigado, ${name}. Conta criada com sucesso.`,
+        placement: 'top',
+        bgColor: 'green.600',
+      });
+      reset();
       
-    });
-    reset();
+    } catch(error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.'
+     
+      toast.show({
+        title: title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
   }
 
   return (
