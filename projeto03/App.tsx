4517612ad70420cd5769f3b217f77ad253a210a4
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-08 11:31:10 -0300

    Deixando a lógica dos contextos somente na interface context

--- projeto03/App.tsx
+++ projeto03/App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { AuthContext } from '@contexts/AuthContext'
+import { AuthContextProvider } from '@contexts/AuthContext'
 
 import { THEME } from 'src/theme';
 import { Loading } from '@components/Loading';
@@ -18,16 +18,9 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      <AuthContext.Provider value={{
-        user: {
-          id: '2',
-          name: 'savi',
-          email: 'savio@email.com',
-          avatar: 'savi.png'
-        } 
-      }}>
+      <AuthContextProvider>
         {fontsLoaded ? <Routes/> : <Loading/>}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- projeto03/src/contexts/AuthContext.tsx
+++ projeto03/src/contexts/AuthContext.tsx
@@ -1,8 +1,27 @@
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext } from "react";
+import { ReactNode, createContext, useContext } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO;
 }
 
+type AuthContextProviderProps = {
+  children: ReactNode;
+}
+
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  return (
+    <AuthContext.Provider value={{
+      user: {
+        id: '2',
+        name: 'savi',
+        email: 'savio@email.com',
+        avatar: 'savi.png'
+      } 
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
