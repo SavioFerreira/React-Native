54750b8317e8a461c5003372e2052e7b63ebcd59
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-11 11:59:10 -0300

    Exibindo detalhes do exercicio

--- projeto03/src/screens/Exercise.tsx
+++ projeto03/src/screens/Exercise.tsx
@@ -1,30 +1,58 @@
-import { HStack, Heading, Icon, VStack, Text, Image, Box, ScrollView } from 'native-base';
+import { useEffect, useState } from 'react';
+import { HStack, Heading, Icon, VStack, Text, Image, Box, ScrollView, useToast } from 'native-base';
 import { TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
-import { Button } from '@components/Button';
+import { AppError } from '@utils/AppError';
+import { api } from '@services/api';
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 import BodySvg from '@assets/body.svg';
 import SeriesSvg from '@assets/series.svg';
 import RepetitionsSvg from '@assets/repetitions.svg';
+
+import { Button } from '@components/Button';
 
 type RouteParamsProps = {
   exerciseId: string;
 }
 
 export function Exercise() {
+  const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO);
   const navigation = useNavigation<AppNavigatorRoutesProps>();
-
   const route = useRoute();
+  const toast = useToast();
 
   const { exerciseId } = route.params as RouteParamsProps;
 
   function handleGoBack() {
     navigation.goBack();
     }
+
+    async function fetchExerciseDetails() {
+      try {
+        const response = await api.get(`/exercises/${exerciseId}`);
+        setExercise(response.data);
+
+      } catch (error) {
+        const isAppError = error instanceof AppError;
+        const title = isAppError ? error.message : 'Não foi possível carregar os detalhes do exercícios';
+  
+        toast.show({
+          title: title,
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      } 
+    }
+
+    useEffect(() => {
+      fetchExerciseDetails();
+    }, [exerciseId]);
+
   return (
     <VStack flex={1}>
       <VStack px={8} bg="gray.600" pt={10}>
@@ -34,43 +62,43 @@ export function Exercise() {
 
         <HStack justifyContent="space-between" mt={6} mb={8} alignItems="center">
           <Heading color="gray.100" fontSize="lg" fontFamily="heading" flexShrink={1}>
-            puxada frontal
+            {exercise.name}
           </Heading>
 
           <HStack alignItems="center">
             <BodySvg />
 
             <Text color="gray.200" ml={1} textTransform="capitalize">
-              Costas
+              {exercise.group}
             </Text>
           </HStack>
         </HStack>
       </VStack>
       <ScrollView>
         <VStack p={8}>
-          <Image 
-            w="full"
-            h={80}
-            source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
-            alt="Nome do exercício"
-            mb={3}
-            resizeMode="cover"
-            rounded="lg"
-          />
-  
+          <Box  rounded="lg" mb={3} overflow="hidden">
+            <Image 
+              w="full"
+              h={80}
+              source={{ uri: `${api.defaults.baseURL}/exercise/demo/${exercise.demo}`}}
+              alt="Nome do exercício"
+              resizeMode="cover"
+            />
+          </Box>
+
           <Box bg="gray.600" rounded="md" pb={4} px={4}>
             <HStack alignItems="center" justifyContent="space-around" mb={5} mt={5}>
               <HStack>
                 <SeriesSvg />
                 <Text color="gray.200" ml={2}>
-                  3 séries
+                  {exercise.series} séries
                 </Text>
               </HStack>
   
               <HStack>
                 <RepetitionsSvg />
                 <Text color="gray.200" ml={2}>
-                  12 repetições
+                {exercise.repetitions} repetições
                 </Text>
               </HStack>
             </HStack>
